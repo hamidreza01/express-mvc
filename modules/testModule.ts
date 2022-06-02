86a55fe9b6bd536e9495f0c8fc9c48d2b5de10d6
hamidreza ghahremani <97837767+hamidreza01@users.noreply.github.com>
2022-06-02 16:06:37 +0430

    Delete modules directory

--- modules/testModule.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const schema = new mongoose.Schema({
-    name : {type : String, required : true},
-    age : {type : Number, required : true},
-})
-export default mongoose.model("testModel", schema,"testModels");
